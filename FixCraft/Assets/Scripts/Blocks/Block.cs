cd22afb12e52e30d8115cceaf4e88783cef66cbb
Milowan <44212243+Milowan@users.noreply.github.com>
2020-01-31 18:45:01 -0500

    added base functionality of blocks and game event manager

---
+++ FixCraft/Assets/Scripts/Blocks/Block.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    private bool mineable;
+
+
+    protected float health;
+
+    public void TakeDamage(float damage)
+    {
+        health -= damage;
+        if (health <= 0.0f)
+            Die();
+    }
+
+    private void Die()
+    {
+        gameObject.SetActive(false);
+    }
+
+}
---
+++ FixCraft/Assets/Scripts/Managers/GameEventManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEventManager
+{
+
+    public delegate void GameEvent();
+
+    public static event GameEvent GameStart;
+    public static event GameEvent GameOver;
+    public static event GameEvent Pause;
+    public static event GameEvent UnPause;
+
+    public static void TriggerGameStart()
+    {
+        if (GameStart != null)
+        {
+            GameStart();
+        }
+    }
+
+    public static void TriggerGameOver()
+    {
+        if (GameOver != null)
+        {
+            GameOver();
+        }
+    }
+
+    public static void TriggerPause()
+    {
+        if (Pause != null)
+        {
+            Pause();
+        }
+    }
+
+    public static void TriggerUnPause()
+    {
+        if (UnPause != null)
+        {
+            UnPause();
+        }
+    }
+
+}
