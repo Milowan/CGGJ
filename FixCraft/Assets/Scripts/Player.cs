bd7bbc9b2806ca15a8f27583cd585d69631c6a3e
eitrunix <52928831+eitrunix@users.noreply.github.com>
2020-01-31 23:30:00 -0500

    Player Update

--- FixCraft/Assets/Scripts/Player.cs
+++ FixCraft/Assets/Scripts/Player.cs
@@ -1,12 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.Animations;
 public class Player : MonoBehaviour
 {
     private Rigidbody2D rb;
     private DrillController drill;
     public int speed = 4;
+    private static int gems = 300;
+    public Animation anim;
+    public int GetGems()
+    {
+        return gems;
+    }
+
+    public void SetGems(int value)
+    {
+        gems = value;
+    }
 
     private List<PlayerState> mCurrentStates = new List<PlayerState>();
 
@@ -24,6 +35,26 @@ public class Player : MonoBehaviour
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
         rb.velocity = new Vector2(horizontal * speed, vertical * speed);
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.GetRayIntersection(ray, 1000);
+            Debug.Log(hit.point);
+        }
+
+        bool found = false;
+        for (int i = 0; i < mCurrentStates.Count - 1; i++)
+        {
+            if (mCurrentStates[i] == PlayerState.MOVING)
+            {
+                found = true;
+            }
+        }
+        if (found == true)
+        {
+            drill.MineBlock();
+            anim.Play("TESTDRILL");
+        }
 
 
         CheckInputs();
@@ -56,6 +87,7 @@ public class Player : MonoBehaviour
                 }
             }
         }
+
         else if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.D))
         {
             bool found = false;
