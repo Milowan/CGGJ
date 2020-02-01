94f5054911364a928caf3adc8d18ce2e6e776100
eitrunix <52928831+eitrunix@users.noreply.github.com>
2020-01-31 23:29:28 -0500

    UI Update

---
+++ FixCraft/Assets/Scripts/UIController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class UIController : MonoBehaviour
+{
+    Player player;
+    public Text gemAmount;
+    int gems;
+
+    private void Awake()
+    {
+        player = FindObjectOfType<Player>();
+        gems = player.GetGems();
+        gemAmount.text = "" + gems;
+
+    }
+}
