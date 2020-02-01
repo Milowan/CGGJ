8bab987430945c32edbee32772dca69a6f84d518
Milowan <44212243+Milowan@users.noreply.github.com>
2020-01-31 19:23:49 -0500

    reinitialized repo

---
+++ FixCraft/Assets/Scripts/Blocks/Steel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Steel : Block
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+}
