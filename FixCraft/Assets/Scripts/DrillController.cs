a64f7ff658b559f491c5f8fe14b09d7a8c58e58b
eitrunix <52928831+eitrunix@users.noreply.github.com>
2020-01-31 21:59:32 -0500

    Player and Drill

---
+++ FixCraft/Assets/Scripts/DrillController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrillController : MonoBehaviour
+{
+    private float speed;
+
+    public void Update()
+    {
+        {
+            Vector3 mousePos = Input.mousePosition;
+            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(mousePos);
+
+            //transform.LookAt(new Vector3(mousePosition.x, mousePosition.y, transform.position.x));
+            Quaternion rot = Quaternion.LookRotation(transform.position - mousePosition, Vector3.forward);
+            transform.rotation = rot;
+            transform.eulerAngles = new Vector3(0, 0, transform.eulerAngles.z);
+
+        }
+    }
+}
--- FixCraft/Assets/Scripts/Player.cs
+++ FixCraft/Assets/Scripts/Player.cs
@@ -4,17 +4,33 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private Rigidbody2D rb;
+    private DrillController drill;
+    public int speed = 4;
+
     private List<PlayerState> mCurrentStates = new List<PlayerState>();
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        drill = GetComponentInChildren<DrillController>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        rb.velocity = new Vector2(horizontal * speed, vertical * speed);
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.GetRayIntersection(ray, 1000);
+            Debug.Log(hit.point);
+        }
+
         CheckInputs();
     }
 
@@ -41,3 +57,4 @@ public class Player : MonoBehaviour
         return mCurrentStates;
     }
 }
+
