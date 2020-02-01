ca361ea721c6aae1b09fa08315d6f8eac40c3784
Sweeny <44808412+MikeSweeny@users.noreply.github.com>
2020-02-01 17:59:03 -0500

    Update Player.cs
    Working on the player facing the direciton he moves. It works, but he can sometimes end up in a permanent tailspin.

--- FixCraft/Assets/Scripts/Player.cs
+++ FixCraft/Assets/Scripts/Player.cs
@@ -15,6 +15,12 @@ public class Player : MonoBehaviour
     [SerializeField]
     private List<PlayerState> mCurrentStates = new List<PlayerState>();
 
+    // looking where youre moving components
+    private GameObject playerSprite;
+    Vector2 previousPos = new Vector2(0, 0);
+    Vector2 moveDirection = new Vector2(0, 0);
+    float angle = 0;
+
     public int GetGems()
     {
         return gems;
@@ -32,8 +38,9 @@ public class Player : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         drill = GetComponentInChildren<DrillController>();
         anim = GetComponentInChildren<Animation>();
-        playerAnimator = GetComponent<Animator>();
+        playerAnimator = transform.GetChild(0).GetComponent<Animator>();
         drillAnimator = drill.GetComponent<Animator>();
+        playerSprite = transform.GetChild(0).gameObject;
     }
 
     // Update is called once per frame
@@ -41,7 +48,7 @@ public class Player : MonoBehaviour
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
-        rb.velocity = new Vector2(horizontal * speed, vertical * speed);
+        rb.velocity = new Vector2(horizontal * speed, vertical * speed);        
 
         CheckInputs();
         UpdateAnimations();
@@ -63,7 +70,6 @@ public class Player : MonoBehaviour
         {
             drill.StopMine();
         }
-
     }
 
     void CheckInputs()
@@ -175,7 +181,17 @@ public class Player : MonoBehaviour
         }
         if (found1 == true)
         {
+            //Set animator bool to true and make the sprite face the forward vector
             playerAnimator.SetBool("bMoving", true);
+            moveDirection = new Vector2(transform.position.x, transform.position.y) - previousPos;
+            if (moveDirection != Vector2.zero)
+            {
+                angle = Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg;
+
+                Quaternion newRotation = Quaternion.AngleAxis((angle - 90), Vector3.forward);
+                playerSprite.transform.rotation = Quaternion.Lerp(transform.rotation, newRotation, 1.0f);
+            }
+            previousPos = transform.position;
         }
         else if (found1 == false)
         {
