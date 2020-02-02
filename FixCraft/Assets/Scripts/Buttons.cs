ef909cb475a95b37807cb3186f106a9da7423107
Grizzly2487 <52928837+Grizzly2487@users.noreply.github.com>
2020-02-02 13:03:37 -0500

    ttf file/intro scene flow structure/textmeshpro
    changes...lots of them.
    
    text mesh pro files
    intro scene flow structure
    new scripts for each video screen
    etc.

---
+++ FixCraft/Assets/Scripts/Buttons.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Buttons : MonoBehaviour
+{
+    public void LoadCutScene()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
---
+++ FixCraft/Assets/Scripts/CutSceneScreen.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class CutSceneScreen : MonoBehaviour
+{
+    public RawImage rawImage;
+    public VideoPlayer videoPlayer;
+    public AudioSource audioSource;
+
+    public GameObject continueText;
+
+    private int time;
+    private int maxTime = 1350;
+    Scene scene;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(PlayGameSceneScreen());
+    }
+    private void Update()
+    {
+        LoadNextScene();
+        time++;
+        if (time >= maxTime)
+        {
+            continueText.SetActive(true);
+        }
+    }
+    IEnumerator PlayGameSceneScreen()
+    {
+        videoPlayer.Prepare();
+        WaitForSeconds waitForSeconds = new WaitForSeconds(1);
+        while (!videoPlayer.isPrepared)
+        {
+            yield return waitForSeconds;
+            break;
+        }
+        rawImage.texture = videoPlayer.texture;
+        videoPlayer.Play();
+        audioSource.Play();
+    }
+    void LoadNextScene()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene(3);
+        }
+    }
+}
