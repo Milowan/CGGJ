8861c86994b9695a7719368dc70422636388b466
Sweeny <44808412+MikeSweeny@users.noreply.github.com>
2020-02-02 15:28:15 -0500

    Update UIController.cs

--- FixCraft/Assets/Scripts/UIController.cs
+++ FixCraft/Assets/Scripts/UIController.cs
@@ -14,14 +14,16 @@ public class UIController : MonoBehaviour
     GameObject[] uiShipSlots = new GameObject[4];
     [SerializeField]
     List<ShipComponent> currentParts = new List<ShipComponent>();
+    GameObject uiTextObject;
 
     private void Start()
     {
         player = Player.GetInstance();
         for (int i = 0; i < uiShipSlots.Length; i++)
         { 
-            uiShipSlots[i] = gameObject.transform.GetChild(3).GetChild(i).gameObject;
+            uiShipSlots[i] = gameObject.transform.GetChild(0).GetChild(i).gameObject;
         }
+        uiTextObject = gameObject.transform.GetChild(0).GetChild(6).GetChild(0).gameObject;
     }
 
     private void Update()
@@ -45,21 +47,25 @@ public class UIController : MonoBehaviour
             {
                 uiShipSlots[i].GetComponent<Image>().sprite = coneSprite;
                 uiShipSlots[i].GetComponent<Image>().color = new Color(255, 255, 255, 255);
+                uiTextObject.GetComponent<Text>().text = "Picked up the nose cone!";
             }
             if (currentParts[i].gameObject.GetComponent<ShipComponent>().GetShipComponentType() == ShipComponentType.ENGINE)
             {
                 uiShipSlots[i].GetComponent<Image>().sprite = engineSprite;
                 uiShipSlots[i].GetComponent<Image>().color = new Color(255, 255, 255, 255);
+                uiTextObject.GetComponent<Text>().text = "Picked up the thruster!";
             }
             if (currentParts[i].gameObject.GetComponent<ShipComponent>().GetShipComponentType() == ShipComponentType.FUEL)
             {
                 uiShipSlots[i].GetComponent<Image>().sprite = fuelSprite;
                 uiShipSlots[i].GetComponent<Image>().color = new Color(255, 255, 255, 255);
+                uiTextObject.GetComponent<Text>().text = "Picked up the fuel tank!";
             }
             if (currentParts[i].gameObject.GetComponent<ShipComponent>().GetShipComponentType() == ShipComponentType.WING)
             {
                 uiShipSlots[i].GetComponent<Image>().sprite = wingSprite;
                 uiShipSlots[i].GetComponent<Image>().color = new Color(255, 255, 255, 255);
+                uiTextObject.GetComponent<Text>().text = "Picked up the wing!";
             }
         }
     }
